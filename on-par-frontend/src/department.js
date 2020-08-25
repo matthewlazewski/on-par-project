b34a48d9b5f9cbaf6fcd9e989b4f1e51add3b861
matthewlazewski <matthew.lazewski@gmail.com>
2020-08-24 21:44:55 -0500

    work on rendering departments

--- on-par-frontend/src/department.js
+++ on-par-frontend/src/department.js
@@ -1,4 +1,5 @@
 class Department {
+    static newDeptRow = document.createElement("div")
     static departmentRow = document.createElement("div")
 
     constructor(name){
@@ -7,12 +8,12 @@ class Department {
 
     static fetchDepartments(){
         fetch(`${BACKEND_URL}/departments`)
-            .then(resp => resp.json)
+            .then(resp => resp.json())
             .then(departments => {
                 this.renderDepts()
-                departments.forEach(department =>{
-                    let dept = new Department(department)
-                    this.departmentRow.innerHTML += dept.renderPage()
+                departments.forEach(d => { 
+                    let dept = new Department(d)
+                    this.departmentRow.innerHTML += dept.renderDeptPage()
                 })
             })
     }
@@ -26,5 +27,36 @@ class Department {
                 let dept = new Department(department)
                 this.departmentRow.innerHTML += dept.renderDept()
             })
+    }
+
+    renderDeptPage(){
+           
+        
+        return 
+            `
+            <div class="dept-page">
+                <h2 class="dept-name">${this.name}</h2>
+            </div>
+            
+            <div class="dept-items">
+                <h4 class="dept-item-title">Total Items</h4>
+                <h3 class="dept-item-number">${this}
+            </div>`
+            
+    }
+
+    static renderDepts(){
+        this.newDeptRow.innerHTML = ""
+        this.newDeptRow.className = "new-dept-row"
+        
+        this.departmentRow.innerHTML = ""
+        this.departmentRow.className = "dept-row"
+
+        container.appendChild(this.newDeptRow)
+        container.appendChild(this.departmentRow)
+
+        this.newDeptRow.innerHTML = `
+        <button type="button" class="btn btn-block btn-outline-primary ml-auto" id="add-league">Add League</button>
+        `
     }
 }

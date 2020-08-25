9b9f1d3eaf1ff39364e338fefb792a36f46e5a1a
matthewlazewski <matthew.lazewski@gmail.com>
2020-08-25 11:34:15 -0500

    started items.js

--- on-par-frontend/src/department.js
+++ on-par-frontend/src/department.js
@@ -9,25 +9,42 @@ class Department {
     static fetchDepartments(){
         fetch(`${BACKEND_URL}/departments`)
             .then(resp => resp.json())          
-            .then(addDepts)
+            .then((response) => {
+                response.data.forEach(dept => {
+                    container.innerHTML += 
+                    `
+                    <div class="dept-page">
+                        <h2 class="dept-name">${dept.attributes.name}</h2>
+                    </div>
+                    
+                    <div class="dept-items">
+                        <h4 class="dept-item-title">Total Items</h4>
+                        <h3 class="dept-item-number">${dept.relationships.items.length || 0}</h3
+                    </div>`
+                })
+            })
         }
     
 
-    static fetchDepartment(departmentId){
-        fetch(`${BACKEND_URL}/departments/${departmentId}`)
-            .then(resp => resp.json())
-            .then(department => {
-                container.innerHTML=""
+    // static fetchDepartment(departmentId){
+    //     fetch(`${BACKEND_URL}/departments/${departmentId}`)
+    //         .then(resp => resp.json())
+    //         .then(department => {
+    //             container.innerHTML=""
 
-                let dept = new Department(department)
-                this.departmentRow.innerHTML += dept.renderDept()
-            })
+    //             let dept = new Department(department)
+    //             this.departmentRow.innerHTML += dept.renderDept()
+    //         })
+    // }
+
+    addDepts(response){
+        response.data.forEach(dept => {
+            renderDeptPage(dept)
+        });
     }
 
-    renderDeptPage(){
-           
-        
-        return 
+    static renderDeptPage(){
+        container.innerHTML += 
             `
             <div class="dept-page">
                 <h2 class="dept-name">${this.name}</h2>
--- on-par-frontend/src/item.js
+++ on-par-frontend/src/item.js
@@ -8,16 +8,19 @@ class Item {
     static fetchItems(){
         fetch(`${BACKEND_URL}/items`)
             .then(resp => resp.json())
-            .then(addItems)
+            .then((response) => {
+                response.data.forEach(item => {
+                    container.innerHTML += `
+                        <div id= "${item.id}">
+                            <li>
+                                <span class="name">${item.attributes.name}</span><br>
+                                <span class="name">On Hand: ${item.attributes.on_hand}</span><br>
+                                <span class="name">Par: ${item.attributes.par}</span>
+                            </li>
+                        `
+                })
+            })
     }
 
-    static addItems(resp){
-        resp.data.forEach(item => {
-            this.addItemToDom(item)
-        });
-    }
-
-    static addItemToDom(item){
-        
-    };
+    
 };

0aeb2a94be83fa7ac74e2095baacac117b4ee6c5
matthewlazewski <matthew.lazewski@gmail.com>
2020-08-25 15:31:27 -0500

    made item form a function

--- on-par-frontend/src/item.js
+++ on-par-frontend/src/item.js
@@ -15,22 +15,53 @@ class Item {
             .then((response) => {
                 this.renderItems()
                 response.data.forEach(item => {
-                    container.innerHTML += `
-                        <div id= "${item.id}">
-                            <li>
-                                <span class="name">${item.attributes.name}</span><br>
-                                <span class="name">On Hand: ${item.attributes.on_hand}</span><br>
-                                <span class="name">Par: ${item.attributes.par}</span>
-                            </li>
-                        `
+                    let newItem = new Item(item)
+                    this.itemRow.innerHTML += newItem.itemPage()
                 })
             })
     }
 
 
     static createNewItem(){
+        let newItem = event.target 
+        let newItemRow = document.querySelector("#new-item-row")
 
+        let formData = {
+            name: form[0].value,
+            department_id: form[1].value,
+            on_hand: form[2].value,
+            par: form[3].value
+        }
+
+        let configObj = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(formData)
+        }
+
+        fetch(`${BACKEND_URL}/items`, configObj)
+            .then(resp => resp.json())
+            .then(item => {
+                form.parentNode.removeChild(form)
+
+                let itemRow = document.querySelector("#item-rows")
+
+                let newItem = new Item(item)
+                itemRow.innerHTML += newItem.itemPage()
+            })
     }
+
+    static newItemEventListener(){
+        let newItemButton = document.querySelector("#add-item")
+        newItemButton.addEventListener("click", () => {
+            event.preventDefault()
+
+        })
+    }
+
 
     static renderItems(){
         this.newItemRow.id = "new-item-row"
@@ -44,10 +75,20 @@ class Item {
         container.appendChild(this.itemRow)
 
         this.newItemRow.innerHTML = `
-            <button type ="button" class="button">Add Item</button>`
+            <button type ="button" class="button" id="new-item">Add Item</button>`
     
     }
 
+    itemPage(){
 
+        return `
+        <div id= "${this.id}">
+            <li>
+                <span class="name">Item Name: ${this.name.attributes.name}</span><br>
+                <span class="name">On Hand: ${this.name.attributes.on_hand}</span><br>
+                <span class="name">Par: ${this.name.attributes.par}</span>
+            </li>
+        `
+    }
     
 };
